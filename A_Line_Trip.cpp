da60ffa09b6536a040ecc6cdbaa9c0828c9c5687
Shaan <shaansingh1118@gmail.com>
2025-02-11 20:51:53 +0530

    800 - problem 2

---
+++ A_Line_Trip.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define ll long long
+const double PI = 3.14159265358979323846;
+const ll MOD = 1e9 + 7;
+template<typename T>T gcd(T x, T y){if(y==0)return x;else return gcd(y,x%y);}
+#define loop(i, a, n) for (int i = a; i < n; i++)
+using namespace std;
+
+int solve(){
+    int n,m; cin >> n >> m;
+ 
+    vector<int> arr(n); 
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    int ans = arr[0];
+    
+    for(int i = 1; i < n; i++){
+        ans = max(arr[i] - arr[i-1], ans);
+    }
+    ans = max(ans, 2*(m - arr[n-1]));
+    return ans;
+}
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    //freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    int t;
+    cin >> t;
+    for(int i = 1; i <= t; i++)
+    {
+        int ans = solve();
+        cout << ans << endl;
+        //cout << (ans ? "YES" : "NO") << endl;
+    //cout << "Case #" << i << ": " <<   << endl;
+    }
+    return 0;
+}
