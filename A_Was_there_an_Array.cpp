d3b834488e13bdbc6534d3aa5121decb4e508023
Shaan <shaansingh1118@gmail.com>
2025-02-22 19:43:40 +0530

    800

---
+++ A_Was_there_an_Array.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define ll long long
+const double PI = 3.14159265358979323846;
+const ll MOD = 1e9 + 7;
+template<typename T>T gcd(T x, T y){if(y==0)return x;else return gcd(y,x%y);}
+#define loop(i, a, n) for (int i = a; i < n; i++)
+using namespace std;
+
+int solve(){
+    int n; cin >> n;
+    vector<int> arr(n,0);
+    for(int i = 1; i < n - 1; i++) cin >> arr[i];
+    for(int i = 2; i < n - 2; i++){
+        if((arr[i] != arr[i-1]) && (arr[i] != arr[i + 1])) return 0;
+    }
+    return 1;
+}
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    //freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    int t;
+    cin >> t;
+    for(int i = 1; i <= t; i++)
+    {
+        int ans = solve();
+      //  cout << ans << endl;
+        cout << (ans ? "YES" : "NO") << endl;
+    //cout << "Case #" << i << ": " <<   << endl;
+    }
+    return 0;
+}
