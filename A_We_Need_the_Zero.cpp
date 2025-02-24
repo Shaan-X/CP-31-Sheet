1cb4f8a878e03cb68f7bc0e33eb65e59f82374ba
Shaan <shaansingh1118@gmail.com>
2025-02-24 19:09:03 +0530

    800

---
+++ A_We_Need_the_Zero.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define ll long long
+const double PI = 3.14159265358979323846;
+const ll MOD = 1e9 + 7;
+template<typename T>T gcd(T x, T y){if(y==0)return x;else return gcd(y,x%y);}
+#define loop(i, a, n) for (int i = a; i < n; i++)
+using namespace std;
+
+int solve(){
+    int n; cin >> n;
+    vector<int> arr(n);
+    loop(i, 0, n) cin >> arr[i];
+    int x = 0;
+    for(int i = 0; i < n; i++){
+        x = x ^ arr[i];
+    }
+    if(x == 0) return 0;
+    if (n % 2 == 1) return x;
+    return -1;
+}
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    //freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    int t;
+    cin >> t;
+    for(int i = 1; i <= t; i++)
+    {
+        int ans = solve();
+        cout << ans << endl;
+        //cout << (ans ? "YES" : "NO") << endl;
+    //cout << "Case #" << i << ": " <<   << endl;
+    }
+    return 0;
+}
