3037910232b7cf73dad57c291f777adcd006a87f
Shaan <shaansingh1118@gmail.com>
2025-02-25 23:37:03 +0530

    800

---
+++ A_Serval_and_Mocha_s_Array.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define ll long long
+const double PI = 3.14159265358979323846;
+const ll MOD = 1e9 + 7;
+template<typename T>T gcd(T x, T y){if(y==0)return x;else return gcd(y,x%y);}
+#define loop(i, a, n) for (int i = a; i < n; i++)
+using namespace std;
+
+int solve(){
+    int n; cin >> n;
+    int flag = 0;
+    vector<int> arr(n);
+    loop(i, 0, n)cin >> arr[i];
+    
+    for(int i = 0; i < n - 1; i++){
+        for(int j = i + 1; j < n; j++){
+            if(__gcd(arr[i], arr[j]) <= 2) flag++;
+        }
+    }
+    
+    return flag;
+}
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    //freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    int t;
+    cin >> t;
+    for(int i = 1; i <= t; i++)
+    {
+        int ans = solve();
+       // cout << ans << endl;
+        cout << (ans ? "YES" : "NO") << endl;
+    //cout << "Case #" << i << ": " <<   << endl;
+    }
+    return 0;
+}
