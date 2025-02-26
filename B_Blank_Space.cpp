bddd1ac169a5c7e1b663bfacf91ffb63cb8226ad
Shaan <shaansingh1118@gmail.com>
2025-02-27 00:29:18 +0530

    800

---
+++ B_Blank_Space.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define ll long long
+const double PI = 3.14159265358979323846;
+const ll MOD = 1e9 + 7;
+template<typename T>T gcd(T x, T y){if(y==0)return x;else return gcd(y,x%y);}
+#define loop(i, a, n) for (int i = a; i < n; i++)
+using namespace std;
+
+int solve(){
+    int n; cin >> n;
+    vector<int> arr(n);
+    loop(i, 0, n) cin >> arr[i];
+    int count = 0, ans = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] == 1){
+            ans = max(ans, count);
+            count = 0;
+        }
+        else count++;
+    }
+    
+    return max(ans, count);
+}
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    //freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    int t;
+    cin >> t;
+    for(int i = 1; i <= t; i++)
+    {
+        int ans = solve();
+        cout << ans << endl;
+        //cout << (ans ? "YES" : "NO") << endl;
+    //cout << "Case #" << i << ": " <<   << endl;
+    }
+    return 0;
+}
