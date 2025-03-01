a4b7309c5a19a47c9d6d82794e2654a18f3d0498
Shaan <shaansingh1118@gmail.com>
2025-03-02 00:35:41 +0530

    800

---
+++ A_Walking_Master.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define ll long long
+const double PI = 3.14159265358979323846;
+#define digits(n) floor(log10(n) + 1);
+const ll MOD = 1e9 + 7;
+template<typename T>T gcd(T x, T y){if(y==0)return x;else return gcd(y,x%y);}
+#define loop(i, a, n) for (int i = a; i < n; i++)
+using namespace std;
+
+int solve(){
+    ll a, b, c, d; 
+    cin >> a >> b >> c >> d;
+    if(d < b) return -1;
+    if(a + abs(d - b) < c) return -1; 
+  
+    return abs(d-b) + abs((a + abs(d - b)) - c);
+}
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    //freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    int t;
+    cin >> t;
+    for(int i = 1; i <= t; i++)
+    {
+        int ans = solve();
+        cout << ans << endl;
+        //cout << (ans ? "YES" : "NO") << endl;
+    //cout << "Case #" << i << ": " <<   << endl;
+    }
+    return 0;
+}
