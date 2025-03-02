07fc6ce3d03b6d492620da743d7d28bb139fddc6
Shaan <shaansingh1118@gmail.com>
2025-03-02 20:23:59 +0530

    Easy

---
+++ A_Goals_of_Victory.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ll long long
+const double PI = 3.14159265358979323846;
+#define digits(n) floor(log10(n) + 1);
+const ll MOD = 1e9 + 7;
+template<typename T>T gcd(T x, T y){if(y==0)return x;else return gcd(y,x%y);}
+#define loop(i, a, n) for (int i = a; i < n; i++)
+using namespace std;
+
+int solve(){
+    int n; cin >> n;
+    int arr[n];
+    int sum = 0;
+    loop(i, 0, n-1) cin >> arr[i], sum += arr[i];  
+    return sum *= -1;
+}
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    //freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    int t;
+    cin >> t;
+    for(int i = 1; i <= t; i++)
+    {
+        int ans = solve();
+        cout << ans << endl;
+        //cout << (ans ? "YES" : "NO") << endl;
+    //cout << "Case #" << i << ": " <<   << endl;
+    }
+    return 0;
+}
