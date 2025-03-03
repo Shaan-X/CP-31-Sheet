6f7cc0bfc65bf6037970ca40c998ef2215e42cf6
Shaan <shaansingh1118@gmail.com>
2025-03-03 12:15:33 +0530

    Used vector of set to include unique pair

--- 900-Rated/A_Forked.cpp
+++ 900-Rated/A_Forked.cpp
@@ -7,6 +7,8 @@ template<typename T>T gcd(T x, T y){if(y==0)return x;else return gcd(y,x%y);}
 #define loop(i, a, n) for (int i = a; i < n; i++)
 using namespace std;
 
+//int dx[4] = {-1, 1, -1, 1}, dy[4] = {-1, -1, 1, 1};
+//look down for another method -- -- --- --- -------------
 int solve(){
     int a, b, xk, yk, xq, yq;
     cin >> a >> b >> xk >> yk >> xq >> yq;
@@ -51,3 +53,33 @@ int main(void)
     }
     return 0;
 }
+
+
+
+
+// --- 2nd method
+
+/*  
+int dx[4] = {-1, 1, -1, 1}, dy[4] = {-1, -1, 1, 1};
+ 
+int main(){
+    int t; cin >> t;
+    for(int i = 0; i < t; i++){
+        int a, b; cin >> a >> b;
+        int x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2;
+        set<pair<int, int>> st1, st2;
+
+        for(int j = 0; j < 4; j++){
+            st1.insert({x1+dx[j]*a, y1+dy[j]*b});
+            st2.insert({x2+dx[j]*a, y2+dy[j]*b});
+            st1.insert({x1+dx[j]*b, y1+dy[j]*a});
+            st2.insert({x2+dx[j]*b, y2+dy[j]*a});
+        }
+
+        int ans = 0;
+        for(auto x : st1)
+            if(st2.find(x) != st2.end())
+                ans++;
+
+    }
+}  */
