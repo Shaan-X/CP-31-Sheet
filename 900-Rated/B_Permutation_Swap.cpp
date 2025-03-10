7e7ff6a8b08e3ed993286893aa6e8dcfd189b9b0
Shaan <shaansingh1118@gmail.com>
2025-03-10 19:37:48 +0530

    * GCD

---
+++ 900-Rated/B_Permutation_Swap.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define ll long long
+const double PI = 3.14159265358979323846;
+#define digits(n) floor(log10(n) + 1);
+const ll MOD = 1e9 + 7;
+template<typename T>T gcd(T x, T y){if(y==0)return x;else return gcd(y,x%y);}
+#define loop(i, a, n) for (int i = a; i < n; i++)
+using namespace std;
+
+//---- gcd calculation -------
+int solve(){
+    int n; cin >> n;
+    vector<int> arr(n);
+    
+    int gici = 0;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+        gici = __gcd(abs(arr[i] - (i+1)), gici);
+    }
+    return gici;
+}
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    //freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    int t;
+    cin >> t;
+    for(int i = 1; i <= t; i++)
+    {
+        int ans = solve();
+        cout << ans << endl;
+        //cout << (ans ? "YES" : "NO") << endl;
+    //cout << "Case #" << i << ": " <<   << endl;
+    }
+    return 0;
+}
