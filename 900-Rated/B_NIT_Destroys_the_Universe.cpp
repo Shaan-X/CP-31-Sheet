971605b382a8dd6e67ca4eb1162066b23f1c6acd
Shaan <shaansingh1118@gmail.com>
2025-03-12 22:59:31 +0530

    To-do

---
+++ 900-Rated/B_NIT_Destroys_the_Universe.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define ll long long
+const double PI = 3.14159265358979323846;
+#define digits(n) floor(log10(n) + 1);
+const ll MOD = 1e9 + 7;
+template<typename T>T gcd(T x, T y){if(y==0)return x;else return gcd(y,x%y);}
+#define loop(i, a, n) for (int i = a; i < n; i++)
+using namespace std;
+
+int solve(){
+    int n; cin >> n;
+    int zero = 0; 
+    vector<int> a(n);
+    loop(i,0,n) {
+        cin >> a[i];
+        if(a[i] == 0) zero++;
+    }
+    if(zero == n) return 0;
+
+    int ans = 0;
+    for(int i = 0; i < n - 1; i++){
+        if(a[i] != 0 && a[i+1] == 0) ans++;
+    }
+    if(a[0] != 0 && ans >= 1) return ans + 1;
+    return max(ans, 1);
+}
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    //freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    int t;
+    cin >> t;
+    for(int i = 1; i <= t; i++)
+    {
+        int ans = solve();
+        cout << ans << endl;
+        //cout << (ans ? "YES" : "NO") << endl;
+    //cout << "Case #" << i << ": " <<   << endl;
+    }
+    return 0;
+}
